24f42b3da9e676b52775407b8bb9c7d4db3af4c6
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-05-20 01:56:25 -0500

    small changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,8 +47,8 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets"
-      execute "mkdir #{shared_path}/tmp/pids"
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
     end
   end
   before :start, :make_dirs

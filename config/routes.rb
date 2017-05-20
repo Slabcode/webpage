a8ff69de2da0a4b504bd67fa82694f7ac13c8745
Andres Gutierrez <agutierrezt@unal.edu.co>
2017-05-20 03:03:03 -0500

    small changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   end
   #get 'statics/index'
   root to: "statics#index"
+  get 'home', to: "statics#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
